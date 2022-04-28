88691112e9e8c675edf78ba61bd23f7197425250
miaskor <misha.skorohodv@gmail.com>
2022-04-28 23:54:02 +0400

    feat(domain): add code that manage version on kotlin
    Signed-off-by: miaskor <misha.skorohodv@gmail.com>

--- gradle/versions.gradle.kts
+++ gradle/versions.gradle.kts
@@ -21,3 +21,30 @@ ext.lib = {
   String artifact ->
   artifact + ':' + artifactsMap[artifact]
 }
+
+/*
+* fun lib(artifact: String): String {
+  return artifactMap[artifact]
+}
+
+
+private fun concatMaps(vararg maps: Map<String, String>) {
+  val map = mapOf<String, String>()
+  maps.forEach {
+    map.plus(it)
+  }
+}
+
+private fun version(version: String, artifacts: List<String>): Map<String, String> {
+  return artifacts.map { it.to(version) }
+}
+
+private val artifactMap = concatMaps(
+  version(
+    "3.15.1", listOf(
+      "org.jooq:jooq-meta",
+      "org.jooq:jooq-codegen"
+    )
+  )
+)
+* */
