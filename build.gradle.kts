f4d59ffd45c1aa26784772eb59a033f1f9958675
miaskor <104252450+miaskor@users.noreply.github.com>
2022-08-05 23:33:49 +0400

    feat(application): add plugin for version control (#48)
    Signed-off-by: m.skorohodv <mikhail.skorokhodov@moneyman.ru>
    
    Co-authored-by: m.skorohodv <mikhail.skorokhodov@moneyman.ru>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 import org.gradle.api.JavaVersion.VERSION_11
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript { plugins.apply("by.miaskor.artifact-versions") }
 plugins {
   id("org.springframework.boot") version "2.6.6"
   id("io.spring.dependency-management") version "1.0.11.RELEASE"
   id("nu.studer.jooq") version "6.0.1"
   id("org.liquibase.gradle") version "2.1.1"
+
   kotlin("jvm") version "1.6.10"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
@@ -20,12 +22,14 @@ subprojects {
   apply {
     plugin("kotlin")
     plugin("org.springframework.boot")
+    plugin("by.miaskor.artifact-versions")
     plugin("io.spring.dependency-management")
   }
   project(":domain:domain") {
     apply {
       plugin("nu.studer.jooq")
       plugin("org.liquibase.gradle")
+      plugin("by.miaskor.mysql-credentials")
     }
   }
 
@@ -39,7 +43,7 @@ subprojects {
   dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.cfg4j:cfg4j-core:4.4.1")
+    implementation(lib("org.cfg4j:cfg4j-core"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   }
