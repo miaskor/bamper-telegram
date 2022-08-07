81cdcfaab73a28605e6a3ade29bc167f7804fb5f
miaskor <misha.skorohodv@gmail.com>
2022-08-07 22:17:00 +0400

    feat(application): add control of plugin version
    Signed-off-by: miaskor <misha.skorohodv@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,26 @@
+pluginManagement {
+  plugins {
+    val versions = File("/home/miaskor/Documents/pet-projects/bamper-telegram-properties/versions.yaml").absoluteFile
+      .readLines()
+
+    fun getPluginVersion(versionForPlugin: String): String {
+      return versions.find { plugin -> plugin.contains("plugin.$versionForPlugin: ") }
+        ?.substringAfter("plugin.$versionForPlugin: ")
+        ?: throw IllegalArgumentException("Plugin $versionForPlugin not found")
+    }
+
+    id("org.springframework.boot") version getPluginVersion("org.springframework.boot")
+    id("io.spring.dependency-management") version getPluginVersion("io.spring.dependency-management")
+    id("nu.studer.jooq") version getPluginVersion("nu.studer.jooq")
+    id("org.liquibase.gradle") version getPluginVersion("org.liquibase.gradle")
+
+    kotlin("jvm") version getPluginVersion("kotlin.jvm")
+    kotlin("plugin.spring") version getPluginVersion("kotlin.plugin.spring")
+    kotlin("plugin.jpa") version getPluginVersion("kotlin.plugin.jpa")
+
+  }
+}
+
 rootProject.name = "bamper-telegram"
 include("bot")
 include("domain:domain-api")
